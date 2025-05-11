fbaabd9eaf6d4916a632abb38df96c0516716aca
Roddorr2 <rodricu0703@gmail.com>
2025-05-11 11:36:41 -0500

    Added two methods for deleting and finding by id

--- src/main/java/com/tailoy/inv/repository/DespachoSucursalProductoRepository.java
+++ src/main/java/com/tailoy/inv/repository/DespachoSucursalProductoRepository.java
@@ -1,5 +1,7 @@
 package com.tailoy.inv.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.tailoy.inv.model.DespachoSucursalProducto;
 
 @Repository
 public interface DespachoSucursalProductoRepository extends JpaRepository<DespachoSucursalProducto, Integer> {
-
+    List<DespachoSucursalProducto> findByDespachoSucursalId(int despachoSucursalId);
+    void deleteByDespachoSucursalId(int despachoSucursalId);
 }
