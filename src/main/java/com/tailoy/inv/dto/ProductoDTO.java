a383879773608beb1db4745b27d59602c07bae87
Roddorr2 <rodricu0703@gmail.com>
2025-05-15 17:13:24 -0500

    changed data types in order to get a new POST JSON object. Now constructor instantiates Producto class

--- src/main/java/com/tailoy/inv/dto/ProductoDTO.java
+++ src/main/java/com/tailoy/inv/dto/ProductoDTO.java
@@ -1,25 +1,48 @@
 package com.tailoy.inv.dto;
 
+import com.tailoy.inv.model.Producto;
 
 public class ProductoDTO {
+    private int id;
+    private int codigo;
     private String nombre;
-    private int subcategoriaId;
+    private String marca;
+    private SubcategoriaDTO subcategoria;
     private int stock;
     private double precioUnitario;
     private String unidadMedida;
-    private boolean estado = true;
+    private boolean estado;
     
     public ProductoDTO() {
+        this.estado = true;
     }
 
-    public ProductoDTO(String nombre, int subcategoriaId, int stock, double precioUnitario, String unidadMedida,
-            boolean estado) {
-        this.nombre = nombre;
-        this.subcategoriaId = subcategoriaId;
-        this.stock = stock;
-        this.precioUnitario = precioUnitario;
-        this.unidadMedida = unidadMedida;
-        this.estado = estado;
+    public ProductoDTO(Producto producto) {
+        this.id = producto.getId();
+        this.codigo = producto.getCodigo();
+        this.nombre = producto.getNombre();
+        this.marca = producto.getMarca();
+        this.subcategoria = new SubcategoriaDTO(producto.getSubcategoria());
+        this.stock = producto.getStock();
+        this.precioUnitario = producto.getPrecioUnitario();
+        this.unidadMedida = producto.getUnidadMedida();
+        this.estado = producto.isEstado();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
     }
 
     public String getNombre() {
@@ -30,12 +53,20 @@ public class ProductoDTO {
         this.nombre = nombre;
     }
 
-    public int getSubcategoriaId() {
-        return subcategoriaId;
+    public String getMarca() {
+        return marca;
     }
 
-    public void setSubcategoriaId(int subcategoriaId) {
-        this.subcategoriaId = subcategoriaId;
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public SubcategoriaDTO getSubcategoria() {
+        return subcategoria;
+    }
+
+    public void setSubcategoria(SubcategoriaDTO subcategoria) {
+        this.subcategoria = subcategoria;
     }
 
     public int getStock() {
@@ -69,7 +100,4 @@ public class ProductoDTO {
     public void setEstado(boolean estado) {
         this.estado = estado;
     }
-
-    
-    
 }
