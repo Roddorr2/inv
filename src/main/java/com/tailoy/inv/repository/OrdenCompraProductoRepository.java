4a557e172dc441921503b321a181c187e607dfda
Roddorr2 <rodricu0703@gmail.com>
2025-05-16 01:41:36 -0500

    Added important methods

--- src/main/java/com/tailoy/inv/repository/OrdenCompraProductoRepository.java
+++ src/main/java/com/tailoy/inv/repository/OrdenCompraProductoRepository.java
@@ -1,5 +1,7 @@
 package com.tailoy.inv.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.tailoy.inv.model.OrdenCompraProducto;
 
 @Repository
 public interface OrdenCompraProductoRepository extends JpaRepository<OrdenCompraProducto, Integer> {
-
+    List<OrdenCompraProducto> findByOrdenCompraId(int ordenCompraId);
+    void deleteByOrdenCompraId(int ordenCompraId);
 }
