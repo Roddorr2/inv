1092b85ea25177303551a6440517368a6844f91c
Roddorr2 <rodricu0703@gmail.com>
2025-05-15 23:54:47 -0500

    Created a DTO class for Categoria to use it for Subcategoria instance

---
+++ src/main/java/com/tailoy/inv/dto/CategoriaDTO.java
@@ -0,0 +1,32 @@
+package com.tailoy.inv.dto;
+
+import com.tailoy.inv.model.Categoria;
+
+public class CategoriaDTO {
+    private int id;
+    private String nombre;
+
+    public CategoriaDTO() {
+    }
+
+    public CategoriaDTO(Categoria categoria) {
+        this.id = categoria.getId();
+        this.nombre = categoria.getNombre();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
