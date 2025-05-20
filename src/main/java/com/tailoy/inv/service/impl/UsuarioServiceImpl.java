47ac5e48036ca1373d3057308a9cb83f738f9d5e
Roddorr2 <rodricu0703@gmail.com>
2025-05-20 13:55:03 -0500

    Implemented all UsuarioService methods

---
+++ src/main/java/com/tailoy/inv/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,10 @@
+package com.tailoy.inv.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.tailoy.inv.service.UsuarioService;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService{
+
+}
