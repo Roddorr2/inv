d2624f6ec6902cc0048fa68cd5152b9f1e1bbb68
Roddorr2 <rodricu0703@gmail.com>
2025-05-20 12:29:38 -0500

    Added new methods to be used on their implementation

--- src/main/java/com/tailoy/inv/repository/ProductoRepository.java
+++ src/main/java/com/tailoy/inv/repository/ProductoRepository.java
@@ -1,5 +1,8 @@
 package com.tailoy.inv.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +10,9 @@ import com.tailoy.inv.model.Producto;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+	Optional<Producto> findByCodigo(int codigo);
+	Optional<Producto> findByMarca(String marca);
+	List<Producto> findByEstadoTrue();
+	List<Producto> findByNombre(String nombre);
+	boolean existsByCodigo(int codigo);
 }
