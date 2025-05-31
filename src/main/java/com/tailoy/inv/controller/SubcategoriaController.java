cbc4330eb1c16d27e2220741b53c8c6baf53cf76
Roddorr2 <rodricu0703@gmail.com>
2025-05-31 01:23:11 -0500

    Created Subcategoria controller for future Angular maintenance

---
+++ src/main/java/com/tailoy/inv/controller/SubcategoriaController.java
@@ -0,0 +1,49 @@
+package com.tailoy.inv.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tailoy.inv.dto.SubcategoriaDTO;
+import com.tailoy.inv.service.impl.SubcategoriaServiceImpl;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/admin/subcategorias")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class SubcategoriaController {
+    private final SubcategoriaServiceImpl subcategoriaService;
+    
+    public SubcategoriaController(SubcategoriaServiceImpl subcategoriaService) {
+        this.subcategoriaService = subcategoriaService;
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<SubcategoriaDTO> registrarSubcategoria(@Validated @RequestBody SubcategoriaDTO  subcategoriaDTO) {
+        SubcategoriaDTO nuevaSubcategoria = subcategoriaService.registrarSubcategoria(subcategoriaDTO);
+        return ResponseEntity.ok(nuevaSubcategoria);
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<SubcategoriaDTO>> listarSubcategorias() {
+        return ResponseEntity.ok(subcategoriaService.listarSubcategorias());
+    }
+    
+    @GetMapping("/existe")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Boolean> existePorNombre(@RequestParam String nombre) {
+        return ResponseEntity.ok(subcategoriaService.existeSubcategoriaPorNombre(nombre));
+    }
+
+}
