4c7eaf4c1bfa502957a4429832eb2e5f010e700e
Roddorr2 <rodricu0703@gmail.com>
2025-06-02 03:04:14 -0500

    Swapped Rest controller url

--- src/main/java/com/tailoy/inv/controller/SubcategoriaController.java
+++ src/main/java/com/tailoy/inv/controller/SubcategoriaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-@RequestMapping("/admin/subcategorias")
+@RequestMapping("/api/subcategorias")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class SubcategoriaController {
     private final SubcategoriaServiceImpl subcategoriaService;
