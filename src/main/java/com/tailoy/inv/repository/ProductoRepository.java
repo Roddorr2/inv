eb57e630e99e03a2dec47046ac43e573f16c9afb
Roddorr2 <rodricu0703@gmail.com>
2025-06-09 02:42:33 -0500

    Used JPQL for the first time

--- src/main/java/com/tailoy/inv/repository/ProductoRepository.java
+++ src/main/java/com/tailoy/inv/repository/ProductoRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.tailoy.inv.model.Producto;
@@ -15,6 +17,9 @@ public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 	List<Producto> findByEstadoTrue();
 	List<Producto> findByNombre(String nombre);
 	boolean existsByCodigo(int codigo);
-	List<Producto> findByNombreContainingIgnoreCaseOrMarcaContainingIgnoreCase(String nombre, String marca);
+	@Query("SELECT p FROM Producto p WHERE " +
+			"LOWER(p.nombre) LIKE LOWER (CONCAT('%', :term, '%')) " +
+			"OR LOWER (p.marca) LIKE LOWER(CONCAT('%', :term, '%'))")
+	List<Producto> findByNombreOrMarcaContainingIgnoreCase(@Param("term") String term);
 	List<Producto> findByNombreContainingIgnoreCaseOrMarcaContainingIgnoreCaseOrCodigo(String nombre, String marca, int codigo);
 }
